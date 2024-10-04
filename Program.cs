0bfb07936cc5f5e365597993b9768cb103d8a4ac
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-10-04 19:24:53 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+ 
+Console.Write("Me diga o que você pensa sobre o aluno que não faz a lição de casa: ");
+Console.ReadLine();
+ 
+Console.WriteLine();
+ 
+Console.Write("Eu sou uma criança educada.No lugar eu diria: ");
+Console.WriteLine(
+Console.ReadLine()!
+.Replace("chata", "#@%*!&")
+.Replace("chato", "#@%*!&")
+.Replace("boba", "#@%*!&")
+.Replace("bobo", "#@%*!&")
+.Replace("feia", "#@%*!&")
+.Replace("feio", "#@%*!&")
+.Replace("boboca", "#@%*!&")
+.Replace("boco", "#@%*!&")
+.Replace("tonta", "#@%*!&")
+.Replace("tonto", "#@%*!&")
+.Replace("palerma", "#@%*!&")
+.Replace("paspalho", "#@%*!&")
+.Replace("tanta", "#@%*!&")
+.Replace("panaca", "#@%*!&")
+.Replace("pentelha", "#@%*!&")
+.Replace("pentelho", "#@%*!&")
+.Replace("burra", "#@%*!&")
+.Replace("burro", "#@%*!&")
+.Replace("besta", "#@%*!&")
+ 
+);
+ 
+ 
+ 
